c80632bebdbc29bf7c2a63d28f612cfc704679c6
Paulus Kutrich <pkutrich@gmail.com>
2019-12-19 01:56:00 -0800

    Add timer and keyboard interrupts.

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,6 +2,26 @@
 
 import sys
 import os
+from datetime import datetime
+import select
+import tty
+import termios
+
+
+class NonBlockingConsole(object):
+
+    def __enter__(self):
+        self.old_settings = termios.tcgetattr(sys.stdin)
+        tty.setcbreak(sys.stdin.fileno())
+        return self
+
+    def __exit__(self, type, value, traceback):
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, self.old_settings)
+
+    def get_data(self):
+        if select.select([sys.stdin], [], [], 0) == ([sys.stdin], [], []):
+            return sys.stdin.read(1)
+        return False
 
 
 class CPU:
@@ -10,14 +30,19 @@ class CPU:
     def __init__(self):
         """Construct a new CPU."""
         self.ram = [0] * 256
-        self.reg = [0x00] * 8
-        self.sp = 0xf4
+        self.reg = [f'{0:08b}'] * 8
+        self.IS = 6
+        self.IM = 5
+        self.SP = 7
+        self.KEY = 0xf4
+        self.reg[self.SP] = 0xf4
         self.pc = 0x00
         self.fl = 0x00
         self.heap_height = 0
-        self.arg_1 = 0xf7
-        self.arg_2 = 0xf6
-
+        self.arg_1 = 0xff
+        self.arg_2 = 0xfe
+        self.interrupts = True
+        # Map functions. {is_alu_function: {sets_pc: {function_code: function}}}
         self.op_map = {1: {0: {0b0000: self.ADD,
                                0b1000: self.AND,
                                0b0111: self.CMP,
@@ -58,9 +83,6 @@ class CPU:
                            }
                        }
 
-    def exit(self):
-        sys.exit()
-
     def load(self):
         """Load a program into memory."""
         args = sys.argv[1:]
@@ -69,7 +91,6 @@ class CPU:
             with open(file, 'r') as f:
                 for line in f:
                     line = line.split('#')[0].strip()
-                    # print('l', line)
                     if line == '':
                         continue
                     self.ram[self.heap_height] = f'{int(line, 2):08b}'
@@ -78,103 +99,152 @@ class CPU:
             raise ValueError('No program file provided.')
 
     def first(self):
+        """Get the value for the first active register."""
         return self.ram_read(self.arg_1)
 
     def second(self):
+        """Get the value for the second active register."""
         return self.ram_read(self.arg_2)
 
     def PRN(self):
-        print(int(self.reg[int(self.first(), 2)], 2), end='\n')
+        """Print a number."""
+        print(int(self.reg[int(self.first(), 2)], 2), end='', flush=True)
 
     def PRA(self):
-        address = int(self.reg[int(self.first(), 2)], 2)
-        letter = self.ram[address]
-        print(chr(int(letter, 2)), end='')
+        """Print a character."""
+        address = self.reg[int(self.first(), 2)]
+        print(f'{chr(int(address, 2))}', end='', flush=True)
 
     def LDI(self):
+        """Load immediate.
+
+        Set a register value."""
         self.reg[int(self.first(), 2)] = self.second()
 
     def HLT(self):
-        self.exit()
+        """Halt program."""
+        sys.exit()
 
     def LD(self):
-        self.reg[int(self.first(), 2)] = self.reg[int(self.second(), 2)]
+        """Load the value in memory at address in arg_2 into register in arg_1"""
+        self.reg[int(self.first(), 2)] = self.ram[int(self.reg[int(self.second(), 2)], 2)]
 
     def PUSH(self):
-        self.sp -= 1
-        if self.sp <= self.heap_height:
-            raise IndexError('Stack Overflow')
-        self.ram[self.sp] = self.reg[int(self.first(), 2)]
+        """Move pointer to next stack position and set value."""
+        self.reg[self.SP] -= 1
+        self.ram[self.reg[self.SP]] = self.reg[int(self.first(), 2)]
 
     def POP(self):
-        self.reg[int(self.first(), 2)] = self.ram[self.sp]
-        self.sp += 1
+        """Get value from top of stack and move pointer."""
+        self.reg[int(self.first(), 2)] = self.ram[self.reg[self.SP]]
+        self.reg[self.SP] += 1
 
     def CALL(self):
-        self.sp -= 1
-        self.ram[self.sp] = self.pc + 1
+        """Store location of pc before jumping to given address."""
+        self.reg[self.SP] -= 1
+        self.ram[self.reg[self.SP]] = self.pc + 1
         self.pc = int(self.reg[int(self.first(), 2)], 2)
 
     def RET(self):
-        self.pc = self.ram[self.sp]
-        self.sp += 1
+        """Return from CALL."""
+        self.pc = self.ram[self.reg[self.SP]]
+        self.reg[self.SP] += 1
 
     def ST(self):
-        self.reg[self.first()] = self.second()
+        """Store value of second register in memory at adress stored in first."""
+        self.ram[int(self.reg[int(self.first(), 2)], 2)] = self.reg[int(self.second(), 2)]
 
     def INT(self):
-        pass
+        """Interrupt program and jump to iterrupt handler.
+
+        Reset IS bits.
+        Halt interrupts.
+        Store all but 8th register in stack, followed by fl and current pc.
+        Move pc to interrupt handler address.
+        """
+        self.reg[self.IS] = '00000000'
+        self.interrupts = False
+        self.reg[self.SP] -= 1
+        self.ram[self.reg[self.SP]] = self.pc
+        self.reg[self.SP] -= 1
+        self.ram[self.reg[self.SP]] = self.fl
+        for i in range(7):
+            self.reg[self.SP] -= 1
+            self.ram[self.reg[self.SP]] = self.reg[i]
+            self.reg[i] = '00000000'
+        self.pc = int(self.ram[int(self.first(), 2)], 2)
 
     def IRET(self):
-        pass
+        """Return from interrupt.
+
+        Restore all but 8th register from stack, followed by fl and pc.
+        """
+        for i in range(6, -1, -1):
+            self.reg[i] = self.ram[self.reg[self.SP]]
+            self.reg[self.SP] += 1
+        self.fl = self.ram[self.reg[self.SP]]
+        self.reg[self.SP] += 1
+        self.pc = self.ram[self.reg[self.SP]]
+        self.reg[self.SP] += 1
+        self.interrupts = True
 
     def NOP(self):
+        """No operation."""
         pass
 
     def JMP(self):
+        """Jump."""
         self.pc = int(self.reg[int(self.first(), 2)], 2)
 
     def JEQ(self):
+        """Jump if equal flag set."""
         if self.fl & 1:
             self.pc = int(self.reg[int(self.first(), 2)], 2)
         else:
             self.pc += 1
 
     def JNE(self):
+        """Jump if equal flag not set."""
         if not self.fl & 1:
             self.pc = int(self.reg[int(self.first(), 2)], 2)
         else:
             self.pc += 1
 
     def JGT(self):
+        """Jump if greater flag set."""
         if self.fl & (1 << 1):
             self.pc = int(self.reg[int(self.first(), 2)], 2)
         else:
             self.pc += 1
 
     def JGE(self):
+        """Jump if greater or equal flags set."""
         if self.fl & 1 or self.fl & (1 << 1):
             self.pc = int(self.reg[int(self.first(), 2)], 2)
         else:
             self.pc += 1
 
     def JLT(self):
+        """Jump if less flag set."""
         if self.fl & (1 << 2):
             self.pc = int(self.reg[int(self.first(), 2)], 2)
         else:
             self.pc += 1
 
     def JLE(self):
+        """Jump if less or equal flags set."""
         if self.fl & 1 or self.fl & (1 << 2):
             self.pc = int(self.reg[int(self.first(), 2)], 2)
         else:
             self.pc += 1
 
     def DEC(self):
+        """Decrement"""
         deced = (int(self.reg[int(self.first(), 2)], 2) - 1) & 0xff
         self.reg[int(self.first(), 2)] = f'{deced:08b}'
 
     def INC(self):
+        """Increment."""
         inced = (int(self.reg[int(self.first(), 2)], 2) + 1) & 0xff
         self.reg[int(self.first(), 2)] = f'{inced:08b}'
 
@@ -183,32 +253,24 @@ class CPU:
         self.reg[int(self.first(), 2)] = f'{added:08b}'
 
     def SUB(self):
+        """Subtract."""
         subbed = (int(self.reg[int(self.first(), 2)], 2) - int(self.reg[int(self.second(), 2)], 2)) * 0xff
         self.reg[int(self.first(), 2)] = f'{subbed:08b}'
 
     def MUL(self):
+        """Multiply."""
         mulled = (int(self.reg[int(self.first(), 2)], 2) * int(self.reg[int(self.second(), 2)], 2)) & 0xff
         self.reg[int(self.first(), 2)] = f'{mulled:08b}'
 
     def DIV(self):
+        """Integer divide."""
         dived = (int(self.reg[int(self.first(), 2)], 2) >> int(self.reg[int(self.second(), 2)], 2)) & 0xff
         self.reg[int(self.first(), 2)] = f'{dived:08b}'
 
     def MOD(self):
+        """Modulus."""
         modded = (int(self.reg[int(self.first(), 2)], 2) % int(self.reg[int(self.second(), 2)], 2)) & 0xff
         self.reg[int(self.first(), 2)] = f'{modded:08b}'
-
-    def CMP(self):
-        comp_a, comp_b = int(self.reg[int(self.first(), 2)], 2), int(self.reg[int(self.second(), 2)], 2)
-        if comp_a == comp_b:
-            self.fl = self.fl & 0b00000001
-            self.fl = self.fl | 0b00000001
-        if comp_a > comp_b:
-            self.fl = self.fl & 0b00000010
-            self.fl = self.fl | 0b00000010
-        if comp_a < comp_b:
-            self.fl = self.fl & 0b00000100
-            self.fl = self.fl | 0b00000100
 
     def AND(self):
         anded = (int(self.reg[int(self.first(), 2)], 2) & int(self.reg[int(self.second(), 2)], 2)) & 0xff
@@ -227,12 +289,34 @@ class CPU:
         self.reg[int(self.first(), 2)] = f'{noted:08b}'
 
     def SHL(self):
+        """Shift left."""
         shled = (int(self.reg[int(self.first(), 2)], 2) << int(self.reg[int(self.second(), 2)], 2)) & 0xff
         self.reg[int(self.first(), 2)] = f'{shled:08b}'
 
     def SHR(self):
-        shred = (int(self.reg[int(self.first(), 2)], 2) << int(self.reg[int(self.second(), 2)], 2)) & 0xff
+        """Shift right."""
+        shred = (int(self.reg[int(self.first(), 2)], 2) >> int(self.reg[int(self.second(), 2)], 2)) & 0xff
         self.reg[int(self.first(), 2)] = f'{shred:08b}'
+
+    def CMP(self):
+        """Make a comparison and set the appropriate fl bit.
+
+        FL bits: 00000LGE
+
+        L Less-than: during a CMP, set to 1 if registerA is less than registerB,
+          zero otherwise.
+        G Greater-than: during a CMP, set to 1 if registerA is greater than
+          registerB, zero otherwise.
+        E Equal: during a CMP, set to 1 if registerA is equal to registerB, zero
+          otherwise.
+        """
+        comp_a, comp_b = int(self.reg[int(self.first(), 2)], 2), int(self.reg[int(self.second(), 2)], 2)
+        if comp_a == comp_b:
+            self.fl = self.fl | 0b00000001
+        if comp_a > comp_b:
+            self.fl = self.fl | 0b00000010
+        if comp_a < comp_b:
+            self.fl = self.fl | 0b00000100
 
     def trace(self):
         """
@@ -241,7 +325,6 @@ class CPU:
         """
 
         print(f"TRACE: pc: {self.pc}, fl: {self.fl}, "
-              # f"ir: {self.ir}, "
               f"ram: {self.ram_read(self.pc)}, "
               f"ram +: {self.ram_read(self.pc + 1)}, ram ++: {self.ram_read(self.pc + 2)},", end='')
 
@@ -260,26 +343,59 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        while True:
-            ir = int(self.ram_read(self.pc), 2)
-            _bytes = ir >> 6
-            _alu = ir & 0b00100000
-            alu = _alu >> 5
-            _adv_pc = ir & 0b00010000
-            adv_pc = _adv_pc >> 4
-            instruction = ir & 0b00001111
-            args = 0xf7
+        # Initialize timer and keyboard listener.
+        then = datetime.now()
+        with NonBlockingConsole() as nbc:
 
-            for _ in range(_bytes):
-                self.pc += 1
-                self.ram_write(args, self.ram_read(self.pc))
-                args -= 1
+            # Continue until HLT reached.
+            while True:
+                args = 0xff  # ram[args] and ram[args + 1] hold active register numbers arg_1 and arg_2.
 
-            if not adv_pc:
-                self.pc += 1
+                if self.interrupts:
+                    # Check time interrupt.
+                    now = datetime.now()
+                    time = (now - then).seconds
+                    if time >= 1:
+                        then = datetime.now()
+                        self.reg[self.IS] = '00000001'
 
-            # print('b', _bytes, 'a', alu,
-            #       'ad', adv_pc, 'ins', bin(ir))
-            # self.trace()
+                    # Check keyboard interrupt.
+                    key = nbc.get_data()
+                    if key:
+                        self.ram_write(self.KEY, f'{ord(key):08b}')
+                        self.reg[self.IS] = '00000010'
 
-            self.op_map[alu][adv_pc][instruction]()
+                        if key == '\x1b':  # x1b is ESC
+                            self.HLT()
+
+                    # Check if any interrupts have been triggered.
+                    mask = int(self.reg[self.IM], 2) & int(self.reg[self.IS], 2)
+                    for i in range(8):
+                        interrupted = (mask >> i) & 1 == 1
+                        if interrupted:
+                            # Write the address of the triggered interrupt vector to ram[args].
+                            self.ram_write(args, f'{(args - 7) + i:08b}')
+                            self.INT()
+                            break
+
+                # Retrieve and decode instruction from memory.
+                instruction = int(self.ram_read(self.pc), 2)
+                _bytes = instruction >> 6  # Number of places to advance pc.
+                alu = (instruction & 0b00100000) >> 5  # 1 if an alu instruction.
+                adv_pc = (instruction & 0b00010000) >> 4  # 1 if instruction advances pc.
+                op_code = instruction & 0b00001111
+
+                # Get required number of bytes from ram.
+                for _ in range(_bytes):
+                    self.pc += 1
+                    self.ram_write(args, self.ram_read(self.pc))
+                    args -= 1
+
+                # Advance the pc if required.
+                if not adv_pc:
+                    self.pc += 1
+
+                # self.trace()
+
+                # Call the operation from the op_map.
+                self.op_map[alu][adv_pc][op_code]()
